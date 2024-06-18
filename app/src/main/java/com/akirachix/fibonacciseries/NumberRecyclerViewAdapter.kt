d3586de9eb32932b6b6ba913266d4fe7fc8e40ce
majidhawa <majidhawaah@gmail.com>
2024-06-18 16:29:01 +0300

    created fibonacci series app

---
+++ app/src/main/java/com/akirachix/fibonacciseries/NumberRecyclerViewAdapter.kt
@@ -0,0 +1,24 @@
+package com.akirachix.fibonacciseries
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NumbersRecyclerViewAdapter (var numbers:List<Int>):RecyclerView.Adapter<NumbersViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumbersViewHolder {
+        val numberView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.fibonacci_items,parent,false)
+        return NumbersViewHolder(numberView)
+    }
+    override fun onBindViewHolder(holder: NumbersViewHolder, position: Int) {
+        holder.tvNumber.text = numbers[position].toString()
+    }
+    override fun getItemCount(): Int {
+        return numbers.size
+    }
+}
+class NumbersViewHolder(numberView:View):RecyclerView.ViewHolder(numberView){
+    val tvNumber = numberView.findViewById<TextView>(R.id.tvNumber)
+}
